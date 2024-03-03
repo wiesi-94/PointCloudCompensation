2e4d91c24b467b34cb305eac1b17b088d8d21ee5
wiesi-94 <83174715+wiesi-94@users.noreply.github.com>
2024-03-03 08:41:32 +0100

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,211 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.BT_AddPTX = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.BT_Read = New System.Windows.Forms.Button()
+        Me.PB_Files = New System.Windows.Forms.ProgressBar()
+        Me.PB_File = New System.Windows.Forms.ProgressBar()
+        Me.Files = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.PropertyGrid1 = New System.Windows.Forms.PropertyGrid()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.BT_Save = New System.Windows.Forms.Button()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip()
+        Me.ToolStripStatusLabel1 = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.StatusStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'BT_AddPTX
+        '
+        Me.BT_AddPTX.Location = New System.Drawing.Point(12, 12)
+        Me.BT_AddPTX.Name = "BT_AddPTX"
+        Me.BT_AddPTX.Size = New System.Drawing.Size(162, 23)
+        Me.BT_AddPTX.TabIndex = 1
+        Me.BT_AddPTX.Text = "Add Files"
+        Me.BT_AddPTX.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.HorizontalScrollbar = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(12, 41)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(162, 214)
+        Me.ListBox1.TabIndex = 0
+        '
+        'BT_Read
+        '
+        Me.BT_Read.Location = New System.Drawing.Point(12, 354)
+        Me.BT_Read.Name = "BT_Read"
+        Me.BT_Read.Size = New System.Drawing.Size(162, 23)
+        Me.BT_Read.TabIndex = 2
+        Me.BT_Read.Text = "RUN"
+        Me.BT_Read.UseVisualStyleBackColor = True
+        '
+        'PB_Files
+        '
+        Me.PB_Files.Location = New System.Drawing.Point(12, 281)
+        Me.PB_Files.Name = "PB_Files"
+        Me.PB_Files.Size = New System.Drawing.Size(162, 23)
+        Me.PB_Files.TabIndex = 20
+        '
+        'PB_File
+        '
+        Me.PB_File.Location = New System.Drawing.Point(12, 325)
+        Me.PB_File.Name = "PB_File"
+        Me.PB_File.Size = New System.Drawing.Size(162, 23)
+        Me.PB_File.TabIndex = 21
+        '
+        'Files
+        '
+        Me.Files.AutoSize = True
+        Me.Files.Location = New System.Drawing.Point(12, 263)
+        Me.Files.Name = "Files"
+        Me.Files.Size = New System.Drawing.Size(30, 15)
+        Me.Files.TabIndex = 22
+        Me.Files.Text = "Files"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(12, 307)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(25, 15)
+        Me.Label9.TabIndex = 23
+        Me.Label9.Text = "File"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 500
+        '
+        'PropertyGrid1
+        '
+        Me.PropertyGrid1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.PropertyGrid1.Location = New System.Drawing.Point(180, 12)
+        Me.PropertyGrid1.Name = "PropertyGrid1"
+        Me.PropertyGrid1.Size = New System.Drawing.Size(361, 336)
+        Me.PropertyGrid1.TabIndex = 24
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(181, 383)
+        Me.TextBox1.Multiline = True
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.ReadOnly = True
+        Me.TextBox1.Size = New System.Drawing.Size(361, 103)
+        Me.TextBox1.TabIndex = 25
+        Me.TextBox1.Text = resources.GetString("TextBox1.Text")
+        '
+        'BT_Save
+        '
+        Me.BT_Save.Location = New System.Drawing.Point(180, 354)
+        Me.BT_Save.Name = "BT_Save"
+        Me.BT_Save.Size = New System.Drawing.Size(361, 23)
+        Me.BT_Save.TabIndex = 26
+        Me.BT_Save.Text = "Save to File"
+        Me.BT_Save.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox2.Location = New System.Drawing.Point(12, 383)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.ReadOnly = True
+        Me.TextBox2.Size = New System.Drawing.Size(163, 103)
+        Me.TextBox2.TabIndex = 27
+        Me.TextBox2.Text = "For PTX files, the application assumes that the first half of the columns represe" &
+    "nts the front view and the second half the back view."
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripStatusLabel1})
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 494)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(553, 22)
+        Me.StatusStrip1.TabIndex = 28
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'ToolStripStatusLabel1
+        '
+        Me.ToolStripStatusLabel1.Name = "ToolStripStatusLabel1"
+        Me.ToolStripStatusLabel1.Size = New System.Drawing.Size(71, 17)
+        Me.ToolStripStatusLabel1.Text = "© Stefan W."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(553, 516)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.BT_Save)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.PropertyGrid1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Files)
+        Me.Controls.Add(Me.PB_File)
+        Me.Controls.Add(Me.PB_Files)
+        Me.Controls.Add(Me.BT_Read)
+        Me.Controls.Add(Me.BT_AddPTX)
+        Me.Controls.Add(Me.ListBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "Compensation"
+        Me.StatusStrip1.ResumeLayout(False)
+        Me.StatusStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents BT_AddPTX As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents BT_Read As Button
+    Friend WithEvents PB_Files As ProgressBar
+    Friend WithEvents PB_File As ProgressBar
+    Friend WithEvents Files As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents PropertyGrid1 As PropertyGrid
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents BT_Save As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents StatusStrip1 As StatusStrip
+    Friend WithEvents ToolStripStatusLabel1 As ToolStripStatusLabel
+End Class
---
+++ Form1.vb
@@ -0,0 +1,450 @@
+﻿Imports System.ComponentModel
+Imports System.Globalization
+Imports System.IO
+Imports IQOPENLib
+
+Public Class Form1
+
+    Dim columns As Integer
+    Dim rows As Integer
+    Dim Points As Integer
+
+    Dim p As New Parameters
+
+    Dim a As Integer
+
+    Dim Pfade As String()
+    Dim FileNumber As Integer
+
+    Dim fertig As Boolean = False
+
+    Private Sub BT_AddPTX_Click(sender As Object, e As EventArgs) Handles BT_AddPTX.Click
+        Dim ofd As New OpenFileDialog()
+        ofd.Filter = "ptx files (*.ptx)|*.ptx|fls files (*.fls)|*.fls|All files (*.*)|*.*"
+        ofd.Multiselect = True
+        If ofd.ShowDialog = DialogResult.OK Then
+            For Each f As String In ofd.FileNames()
+                If IO.File.Exists(f) Then
+                    ListBox1.Items.Add(f)
+                Else
+                    MsgBox("File does not exist")
+                End If
+            Next
+        End If
+    End Sub
+
+    Private Sub BT_Read_Click(sender As Object, e As EventArgs) Handles BT_Read.Click
+        fertig = False
+        BT_Read.Enabled = False
+        BT_AddPTX.Enabled = False
+        FileNumber = 0
+
+        ReDim Pfade(ListBox1.Items.Count - 1)
+        For File = 0 To ListBox1.Items.Count - 1
+            Pfade(File) = ListBox1.Items(File)
+        Next
+
+        PB_Files.Maximum = ListBox1.Items.Count
+
+        Dim t As New System.Threading.Thread(AddressOf Run)
+        t.Start()
+
+    End Sub
+
+    Private Sub Run()
+        For Each f In Pfade
+            FileNumber += 1
+            If f.EndsWith("fls") Then
+                CalculateAndApllyFaro(f, "-" & Now.ToString("dd.MM.yyyy"))
+            Else
+                CalculateAndAplly(f, "-" & Now.ToString("dd.MM.yyyy"))
+            End If
+
+        Next
+        fertig = True
+    End Sub
+
+    Private Sub CalculateAndAplly(ByVal Pfad As String, ByVal Prosfix As String)
+        Dim TransMatrix(7) As String
+
+        'Read File
+        Dim fs As New IO.FileStream(Pfad, IO.FileMode.Open)
+        Dim sr As New IO.StreamReader(fs)
+
+        columns = sr.ReadLine()
+        rows = sr.ReadLine()
+
+        Points = columns * rows
+
+        Dim x(Points - 1) As Double
+        Dim y(Points - 1) As Double
+        Dim z(Points - 1) As Double
+        Dim i(Points - 1) As String
+
+
+        For a = 0 To 7
+            TransMatrix(a) = sr.ReadLine
+        Next
+
+        Dim c As Integer = 0
+        Dim ar() As String
+        a = 0
+        Do Until sr.EndOfStream
+            a += 1
+            ar = sr.ReadLine.Split(" ")
+            x(c) = Double.Parse(ar(0), CultureInfo.InvariantCulture)
+            y(c) = Double.Parse(ar(1), CultureInfo.InvariantCulture)
+            z(c) = Double.Parse(ar(2), CultureInfo.InvariantCulture)
+
+            i(c) = ar(3) & " " & ar(4) & " " & ar(5) & " " & ar(6)
+
+            c += 1
+        Loop
+        sr.Close()
+        fs.Close()
+
+
+        Dim Roh As Double
+        Dim Alpha As Double
+        Dim Theta As Double
+
+        For a = 0 To Points - 1
+
+            Roh = Math.Sqrt(x(a) ^ 2 + y(a) ^ 2 + z(a) ^ 2)
+            Alpha = Math.Atan(z(a) / Math.Sqrt(x(a) ^ 2 + y(a) ^ 2))
+            Theta = Math.Atan2(y(a) * -1, x(a))
+
+            If a > Points / 2 Then 'Secound scanner half
+                Alpha = Math.PI - Alpha
+                Theta -= Math.PI
+            End If
+
+
+            Theta -= p.B1 * Theta + p.B2 * Math.Sin(Theta) + p.B3 * Math.Cos(Theta) + p.B4 * Math.Sin(2 * Theta) + p.B5 * Math.Cos(2 * Theta) _
+                + p.B6 * Sec(Alpha) + p.B7 * Math.Tan(Alpha) + p.B8 * rows ^ -1 + p.B9 * Math.Sin(Alpha) + p.B10 * Math.Cos(Alpha) 'Delta Theta
+
+
+            Alpha -= p.C0 + p.C1 * Alpha + p.C2 * Math.Sin(Alpha) + p.C3 * Math.Sin(2 * Alpha) + p.C4 * Math.Cos(2 * Alpha) + p.C5 * Roh ^ -1 _
+                + p.C6 * Math.Sin(2 * Theta) + p.C7 * Math.Cos(2 * Theta) + p.C8 * Math.Sin(4 * Theta) 'Delta alpha
+
+            Roh -= p.A0 + p.A1 * Roh + p.A2 * Math.Sin(Alpha) 'Delta roh
+            If p.U1 > 0 Then
+                Roh -= p.A3 * Math.Sin(((4 / Math.PI) / p.U1) * Roh) + p.A4 * Math.Cos(((4 / Math.PI) / p.U1) * Roh) 'Delta roh, only when u1 is not 0
+            End If
+
+
+            Alpha -= 1.570796326795
+
+            x(a) = (Roh) * Math.Sin(Alpha) * Math.Cos(Theta) * -1
+            y(a) = (Roh) * Math.Sin(Alpha) * Math.Sin(Theta)
+            z(a) = (Roh) * Math.Cos(Alpha)
+
+            If Double.IsNaN(x(a)) Then x(a) = 0
+            If Double.IsNaN(y(a)) Then y(a) = 0
+            If Double.IsNaN(z(a)) Then z(a) = 0
+
+
+        Next
+
+
+
+        'Write new File
+        fs = New IO.FileStream(Pfad.Substring(0, Pfad.Length - 4) & Prosfix & ".ptx", IO.FileMode.CreateNew)
+        Dim sw As New IO.StreamWriter(fs)
+
+        sw.Write((columns & vbLf))
+        sw.Write((rows & vbLf))
+        For a = 0 To 7
+            sw.Write(TransMatrix(a) & vbLf)
+        Next
+
+        Dim fp = New System.Globalization.CultureInfo("en-US")
+
+        For a = 0 To Points - 1
+            sw.Write(x(a).ToString("N5", fp) & " " & y(a).ToString("N5", fp) & " " & z(a).ToString("N5", fp) & " " & i(a) & vbLf)
+        Next
+
+        sw.Close()
+        fs.Close()
+    End Sub
+
+    Private Sub CalculateAndApllyFaro(ByVal Pfad As String, ByVal Prosfix As String)
+        Dim fp = New System.Globalization.CultureInfo("en-US") 'decimal point
+
+        'Read File
+        Dim licenseCode = "FARO Open Runtime License" & vbLf &
+                "Key:xxxxxxxxxxxxxxxxxxxxxx" & vbLf &
+                vbLf &
+                "The software is the registered property " &
+                "of FARO Scanner Production GmbH, " &
+                "Stuttgart, Germany." & vbLf &
+                "All rights reserved." & vbLf &
+                "This software may only be used with " &
+                "written permission of FARO Scanner " &
+                "Production GmbH, Stuttgart, Germany."
+
+        Dim licLibIf As IiQLicensedInterfaceIf = New iQLibIfClass()
+        licLibIf.License = licenseCode
+        Dim libRef As IiQLibIf = CType(licLibIf, IiQLibIf)
+        libRef.load(Pfad)
+
+        rows = libRef.getScanNumRows(0)
+        columns = libRef.getScanNumCols(0)
+        libRef.scanReflectionMode = 0
+
+
+        Dim EndFrontSight = libRef.getScanAnglesIf(0).ColEndFrontSight
+        'MsgBox(libRef.getRootObject.getSphereObjSpeci)
+
+        Dim TransMatrix(7) As String
+
+        Points = columns * rows
+
+        Dim x(Points - 1) As Double
+        Dim y(Points - 1) As Double
+        Dim z(Points - 1) As Double
+        Dim i(Points - 1) As Short
+
+        Dim r(Points - 1) As Double
+        Dim alp(Points - 1) As Double
+        Dim the(Points - 1) As Double
+
+
+        'Dim fs As New IO.FileStream(Pfad, IO.FileMode.Open)
+        'Dim sr As New IO.StreamReader(fs)
+
+        TransMatrix(0) = "0.00000000 0.00000000 0.00000000"
+        TransMatrix(1) = "1.00000000 0.00000000 0.00000000"
+        TransMatrix(2) = "0.00000000 1.00000000 0.00000000"
+        TransMatrix(3) = "0.00000000 0.00000000 1.00000000"
+        TransMatrix(4) = "1.00000000 0.00000000 0.00000000 0.00000000"
+        TransMatrix(5) = "0.00000000 1.00000000 0.00000000 0.00000000"
+        TransMatrix(6) = "0.00000000 0.00000000 1.00000000 0.00000000"
+        TransMatrix(7) = "0.00000000 0.00000000 0.00000000 1.00000000"
+
+
+        Dim c As Integer = 0
+        Dim ar() As String
+
+        Dim pos As Double()
+        Dim int As Integer()
+
+
+        For co = 0 To columns - 1
+
+            'libRef.getXYZScanPoints2(0, 0, co, rows, pos, int) ' Read full column
+            'For ro = 0 To rows - 1
+            '    a = co * rows + ro
+            '    x(a) = pos(ro * 3)
+            '    y(a) = pos(ro * 3 + 1)
+            '    z(a) = pos(ro * 3 + 2)
+
+            '    i(a) = int(ro)
+            'Next
+
+            libRef.getPolarScanPoints2(0, 0, co, rows, pos, int) ' Read full column Polar
+            For ro = 0 To rows - 1
+                a = co * rows + ro
+                r(a) = pos(ro * 3)
+                the(a) = pos(ro * 3 + 1)
+                alp(a) = pos(ro * 3 + 2)
+            Next
+
+        Next
+
+        libRef.unloadScan(0)
+
+
+        Dim Roh As Double
+        Dim Alpha As Double
+        Dim Theta As Double
+
+        For a = 0 To Points - 1
+
+            Roh = Math.Sqrt(x(a) ^ 2 + y(a) ^ 2 + z(a) ^ 2)
+            Alpha = Math.Atan(z(a) / Math.Sqrt(x(a) ^ 2 + y(a) ^ 2))
+            Theta = Math.Atan2(y(a) * -1, x(a))
+
+            Roh = r(a)
+            Alpha = alp(a)
+            Theta = the(a)
+
+            If a / rows > EndFrontSight Then 'Secound scanner half
+                Alpha = Math.PI - Alpha
+                Theta -= Math.PI
+            End If
+
+
+
+            Theta -= p.B1 * Theta + p.B2 * Math.Sin(Theta) + p.B3 * Math.Cos(Theta) + p.B4 * Math.Sin(2 * Theta) + p.B5 * Math.Cos(2 * Theta) _
+                + p.B6 * Sec(Alpha) + p.B7 * Math.Tan(Alpha) + p.B8 * Roh ^ -1 + p.B9 * Math.Sin(Alpha) + p.B10 * Math.Cos(Alpha) 'Delta Theta
+
+
+
+            Alpha -= p.C0 + p.C1 * Alpha + p.C2 * Math.Sin(Alpha) + p.C3 * Math.Sin(2 * Alpha) + p.C4 * Math.Cos(2 * Alpha) + p.C5 * Roh ^ -1 _
+                + p.C6 * Math.Sin(2 * Theta) + p.C7 * Math.Cos(2 * Theta) + p.C8 * Math.Sin(4 * Theta) 'Delta alpha
+
+            Roh -= p.A0 + p.A1 * Roh + p.A2 * Math.Sin(Alpha) 'Delta roh
+            If p.U1 > 0 Then
+                Roh -= p.A3 * Math.Sin(((4 / Math.PI) / p.U1) * Roh) + p.A4 * Math.Cos(((4 / Math.PI) / p.U1) * Roh) 'Delta roh, only when u1 is not 0
+            End If
+
+
+            Alpha -= 1.570796326795
+
+            x(a) = (Roh) * Math.Sin(Alpha) * Math.Cos(Theta) * -1
+            y(a) = (Roh) * Math.Sin(Alpha) * Math.Sin(Theta)
+            z(a) = (Roh) * Math.Cos(Alpha)
+
+            If Double.IsNaN(x(a)) Then x(a) = 0
+            If Double.IsNaN(y(a)) Then y(a) = 0
+            If Double.IsNaN(z(a)) Then z(a) = 0
+
+
+        Next
+
+
+
+        'Write new File
+        Dim fs = New IO.FileStream(Pfad.Substring(0, Pfad.Length - 4) & Prosfix & ".ptx", IO.FileMode.CreateNew)
+        Dim sw As New IO.StreamWriter(fs)
+
+        sw.Write((columns & vbLf))
+        sw.Write((rows & vbLf))
+        For a = 0 To 7
+            sw.Write(TransMatrix(a) & vbLf)
+        Next
+
+
+        Dim int255 As Short
+        For a = 0 To Points - 1
+            int255 = 255 * (i(a) / 2047)
+            sw.Write(x(a).ToString("N5", fp) & " " & y(a).ToString("N5", fp) & " " & z(a).ToString("N5", fp) & " " &
+                     (1 / 2047 * i(a)).ToString("N5", fp) & " " & int255 & " " & int255 & " " & int255 & vbCrLf)
+        Next
+
+        sw.Close()
+        fs.Close()
+    End Sub
+
+    Private Function Sec(angle As Double) As Double
+        ' Calculate the secant of angle, in radians.
+        Return 1.0 / Math.Cos(angle)
+    End Function
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        PB_Files.Value = FileNumber
+        PB_File.Maximum = Points
+        PB_File.Value = a
+
+        If fertig Then
+            BT_AddPTX.Enabled = True
+            BT_Read.Enabled = True
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim x As New System.Xml.Serialization.XmlSerializer(p.GetType)
+        Dim fs As New FileStream("Parameter.xml", FileMode.Open)
+        p = x.Deserialize(fs)
+        fs.Close()
+        PropertyGrid1.SelectedObject = p
+    End Sub
+
+    Private Sub BT_Save_Click(sender As Object, e As EventArgs) Handles BT_Save.Click
+        Dim x As New System.Xml.Serialization.XmlSerializer(p.GetType)
+        Dim fs As New FileStream("Parameter.xml", FileMode.Create)
+        x.Serialize(fs, p)
+        fs.Close()
+    End Sub
+
+End Class
+
+
+Public Class Parameters
+
+
+
+    <Category("A-Rangefinder"), Description("A0 describes the rangefinder offset")>
+    Public Property A0 As Double
+
+    <Category("A-Rangefinder"), Description("A1 describes the scale factor error")>
+    Public Property A1 As Double
+
+    <Category("A-Rangefinder"), Description("A2 describes the laser axis vertical offset")>
+    Public Property A2 As Double
+
+    <Category("A-Rangefinder"), Description("A3 and A4 describe the cyclic errors")>
+    Public Property A3 As Double
+
+    <Category("A-Rangefinder"), Description("A3 and A4 describe the cyclic errors")>
+    Public Property A4 As Double
+
+    <Category("A-Rangefinder"), Description("U1 describes the period(m) of the cyclic error")>
+    Public Property U1 As Double
+
+
+
+
+    <Category("B-Theta (vertical axis)"), Description("B1 describes the scale factor error")>
+    Public Property B1 As Double
+
+    <Category("B-Theta (vertical axis)"), Description("B2 and B3 describe the horizontal circle eccentricity")>
+    Public Property B2 As Double
+
+    <Category("B-Theta (vertical axis)"), Description("B2 and B3 describe the horizontal circle eccentricity ")>
+    Public Property B3 As Double
+
+    <Category("B-Theta (vertical axis)"), Description("B4 and B5 describe the non-orthogonality of encoder and vertical axis")>
+    Public Property B4 As Double
+
+    <Category("B-Theta (vertical axis)"), Description("B4 and B5 describe the non-orthogonality of encoder and vertical axis")>
+    Public Property B5 As Double
+
+    <Category("B-Theta (vertical axis)"), Description("B6 describes the collimation axis error")>
+    Public Property B6 As Double
+
+    <Category("B-Theta (vertical axis)"), Description("B7 describes the trunnion axis error")>
+    Public Property B7 As Double
+
+    <Category("B-Theta (vertical axis)"), Description("B8 describes horizontal eccentricity of collimation axis")>
+    Public Property B8 As Double
+
+    <Category("B-Theta (vertical axis)"), Description("B9 and B10 describe the trunnion axis wobble")>
+    Public Property B9 As Double
+
+    <Category("B-Theta (vertical axis)"), Description("B9 and B10 describe the trunnion axis wobble")>
+    Public Property B10 As Double
+
+
+
+
+    <Category("C-Alpha (Horizontal axis)"), Description("C0 describes the vertical circle index error")>
+    Public Property C0 As Double
+
+    <Category("C-Alpha (Horizontal axis)"), Description("C1 describes the scale factor error ")>
+    Public Property C1 As Double
+
+    <Category("C-Alpha (Horizontal axis)"), Description("C2 describes the vertical circle eccentricity")>
+    Public Property C2 As Double
+
+    <Category("C-Alpha (Horizontal axis)"), Description("C3 and C4 describe the non-orthogonality of encoder and trunnion axis")>
+    Public Property C3 As Double
+
+    <Category("C-Alpha (Horizontal axis)"), Description("C3 and C4 describe the non-orthogonality of encoder and trunnion axis")>
+    Public Property C4 As Double
+
+    <Category("C-Alpha (Horizontal axis)"), Description("C5 describes the vertical eccentricity of collimation axis")>
+    Public Property C5 As Double
+
+    <Category("C-Alpha (Horizontal axis)"), Description("C6, C7, and C8 describe the vertical axis wobble")>
+    Public Property C6 As Double
+
+    <Category("C-Alpha (Horizontal axis)"), Description("C6, C7, and C8 describe the vertical axis wobble")>
+    Public Property C7 As Double
+
+    <Category("C-Alpha (Horizontal axis)"), Description("C6, C7, and C8 describe the vertical axis wobble")>
+    Public Property C8 As Double
+
+End Class
